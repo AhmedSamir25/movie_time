e0c4019a019577671adeb05e180f230679f16f41
ahmed <ahmedsami.r@yahoo.com>
2023-11-01 14:02:44 +0200

    start app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieTime());
+}
+
+class MovieTime extends StatelessWidget {
+  const MovieTime({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
